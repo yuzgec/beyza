f1c68eceb89d1e2fc0c611539fbbd1b0119b8f42
yuzgec <olcayy@gmail.com>
2024-12-17 14:13:49 +0300

    Fix

--- resources/views/frontend/service/index.blade.php
+++ resources/views/frontend/service/index.blade.php
@@ -1,22 +1,21 @@
 @extends('frontend.layout.app')
 @section('content')
 
-    <section class="page-header page-header-modern bg-color-quaternary page-header-md custom-page-header">
-        <div class="container">
-            <div class="row mt-3">
-                <div class="col-md-8 order-2 order-md-1 align-self-center p-static">
-                    <h1>- ÇALIŞMA ALANLARIMIZ</h1>
-                    <span class="d-block text-4">Uzm. Dr. Turan Çetin</span>
-                </div>
-                <div class="col-md-4 order-1 order-md-2 align-self-center">
-                    <ul class="breadcrumb d-block text-md-end breadcrumb-light">
-                        <li><a href="{{ route('home') }}">Anasayfa</a></li>
-                        <li class="active">ÇALIŞMA ALANLARIMIZ</li>
-                    </ul>
-                </div>
+<section class="page-header page-header-modern bg-color-grey page-header-lg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+                <h1 class="text-color-dark font-weight-bold">Çalışma Alanlarımız</h1>
+            </div>
+            <div class="col-md-4 order-1 order-md-2 align-self-center">
+                <ul class="breadcrumb d-flex justify-content-md-end text-3-5">
+                    <li><a href="{{ route('home')}}" class="text-color-default text-color-hover-primary text-decoration-none">Anasayfa</a></li>
+                    <li class="active">Çalışma Alanlarımız</li>
+                </ul>
             </div>
         </div>
-    </section>
+    </div>
+</section>
     <div class="container container-fluid">
         <div class="row">
             @foreach($Service as $item)
@@ -25,7 +24,7 @@
 
                     <span class="thumb-info thumb-info-no-borders thumb-info-no-borders-rounded thumb-info-lighten thumb-info-bottom-info thumb-info-bottom-info-dark thumb-info-bottom-info-show-more thumb-info-no-zoom">
                         <span class="thumb-info-wrapper">
-                            <img src="{{ (!$item->getFirstMediaUrl('page')) ? '/resimyok.jpg' : $item->getFirstMediaUrl('page', 'thumb') }}" class="img-fluid" alt="{{ $item->title }} - Dr. Turan Çetin">
+                            <img src="{{ (!$item->getFirstMediaUrl('page')) ? '/resimyok.jpg' : $item->getFirstMediaUrl('page', 'thumb') }}" class="img-fluid" alt="Eskişehir {{ $item->title }}">
                             <span class="thumb-info-title">
                                 <span class="thumb-info-inner line-height-1">{{ $item->title }}</span>
 {{--

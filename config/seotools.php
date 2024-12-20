daa93326852a0165d1f4d2286bf87a4266559ad4
yuzgec <olcayy@gmail.com>
2024-12-20 17:28:38 +0300

    Fix

--- config/seotools.php
+++ config/seotools.php
@@ -9,9 +9,9 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => "Eskişehir Psyche Psikoloji Kliniği", // set false to total remove
+            'title'        => "Eskişehir Odunpazarı Psikoloji Kliniği", // set false to total remove
             'titleBefore'  => false, // Put defaults.title before page title, like 'Eskişehir Psyche Psikoloji Kliniği - Dashboard'
-            'description'  => 'Eskişehir Psyche Psikoloji Kliniği', // set false to total remove
+            'description'  => 'Psyche Psikoloji Eskişehir Odunpazarında uzman psikolog ve psikodiyet alanlarında hizmet vermektedir.  ', // set false to total remove
             'separator'    => ' - ',
             'keywords'     => [],
             'canonical'    => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
@@ -20,9 +20,8 @@ return [
         /*
          * Webmaster tags are always added.
          */
-
         'webmaster_tags' => [
-            'google'    => null,
+            'google'    => 'UVSN4CJaP3f8p76cqvyvbZFATzZLJ7_ClkiOglQ0_as',
             'bing'      => null,
             'alexa'     => null,
             'pinterest' => null,
@@ -37,8 +36,8 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'Eskişehir Psyche Psikoloji Kliniği', // set false to total remove
-            'description' => 'Eskişehir Psyche Psikoloji Kliniği', // set false to total remove
+            'title'        => "Eskişehir Odunpazarı Psikoloji Kliniği", // set false to total remove
+            'description'  => 'Psyche Psikoloji Eskişehir Odunpazarında uzman psikolog ve psikodiyet alanlarında hizmet vermektedir.  ', // set false to total remove
             'url'         => false, // Set null for using Url::current(), set false to total remove
             'type'        => false,
             'site_name'   => false,
@@ -59,8 +58,8 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Eskişehir Psyche Psikoloji Kliniği', // set false to total remove
-            'description' => 'Eskişehir Psyche Psikoloji Kliniği', // set false to total remove
+            'title'        => "Eskişehir Odunpazarı Psikoloji Kliniği", // set false to total remove
+            'description'  => 'Psyche Psikoloji Eskişehir Odunpazarında uzman psikolog ve psikodiyet alanlarında hizmet vermektedir.  ', // set false to total remove
             'url'         => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
             'type'        => 'WebPage',
             'images'      => [],
